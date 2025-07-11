7722280c0e20ce51faf23a16c192077de5d2297c
TarushiV26 <tarushiv2611@gmail.com>
2025-07-11 10:31:07 +0530

    Create program2.cpp

---
+++ program2.cpp
@@ -0,0 +1,11 @@
+//program2:inputting to numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter first number: ";
+    cin>>num1;
+    cout<<"enter second number: ";
+    cin>>num2;
+}
